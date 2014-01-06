0ff4a4d8be08b0cd969038ff928052882081e892
Mirtl <bubuzoglo@gmail.com>
2014-01-06 15:21:00 +0400

    trying to make sliding effect with css transitions

--- panel.js
+++ panel.js
@@ -4,14 +4,21 @@ $(document).ready(function(){
         var pos = foo.scrollTop();
         $(".stufffar").css("top", (pos/4.5 + 230) + "px");
         $(".stuffclose").css("top", (pos/2 + 230) + "px");
-        if(pos === 2567){
+        if(pos === 2572){
             $(".front").css("background-position", -215 + "px")
         }
         else{
             $(".front").css("background-position", 0 + "px")
         }
     }
+    function remImportant(){
+        $("body").removeClass("fuckchrome");
+    }
     $(window).scroll(function(){
         update();
-    })
+    });
+    $("li").click(function(){
+        remImportant();
+    });
+
 })
